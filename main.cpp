239883fb131198d336faa34c44e1425ec4617313
Максим <maksym0407@MacBook-Pro-Maksim.local>
2026-09-21 22:54:34 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#include <string>
+int main() {
+string name = ("Maksym");
+    int course = 1;
+    cout << "Hi, " << name  << " .Course : " << course << endl;
+    cin.get();
+    return 0;
+}
